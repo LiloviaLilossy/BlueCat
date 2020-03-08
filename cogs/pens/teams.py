c56b184e2877aed94317afa18fcd559a041c51ae
LiloviaLilossy <ddlc.femc@gmail.com>
2020-03-08 21:29:36 +0300

    aaaaaaaaaaa

---
+++ cogs/pens/teams.py
@@ -0,0 +1,68 @@
+import json
+from discord.ext import commands
+
+class StarColorPensTeams(commands.Cog, name="Princesses' Star Color Pens: Teams"):
+    def __init__(self, bot):
+        self.bot = bot
+
+<<<<<<< HEAD
+    @commands.guild_only()
+    @commands.bot_has_permissions(manage_roles=True)
+    @commands.command(name="team-join")
+    async def team_join(self, ctx, *, name=None):
+        try:
+            file = open(f"guild-settings/{ctx.guild.id}/colorpens.json", "r")
+        except FileNotFoundError:
+            return await ctx.send("Princesses' Star Color Pens is disabled here-nyan.")
+        if name == None:
+            return await ctx.send("You know, you can't join nothing. Maybe you'll say, which team do you want to join-nyan?")
+        data = json.load(file)
+        file.close()
+        try:
+            team = data["Teams"][name]
+        except KeyError:
+            return await ctx.send("There are no team with that name here-nyan.")
+        await ctx.send(f"Okay-nyan. <@"+str(team["Leader"])+">, there are a newbie. Do you want to accept them-nyan?")
+        def check(m):
+            return m.author.id == team["Leader"] and m.channel == ctx.channel
+        answer = await self.bot.wait_for('message', check=check)
+        if "yes" in answer.content:
+            role = ctx.guild.get_role(team["TeamRole"])
+            await ctx.author.add_roles(role)
+            await ctx.send(f"Congrats and welcome to the {name} team-nyan!")
+        else:
+            await ctx.send(f"Sorry, {ctx.author.mention}, but <@"+str(team["Leader"])+"> said you can't.")
+
+def setup(bot):
+    bot.add_cog(StarColorPensTeams(bot))
+=======
+	@commands.guild_only()
+	@commands.bot_has_permissions(manage_roles=True)
+	@commands.command(name="team-join")
+	async def team_join(self, ctx, *, name=None):
+		try:
+			file = open(f"guild-settings/{ctx.guild.id}/colorpens.json", "r")
+		except FileNotFoundError:
+			return await ctx.send("Princesses' Star Color Pens is disabled here-nyan.")
+		if name == None:
+			return await ctx.send("You know, you can't join nothing. Maybe you'll say, which team do you want to join-nyan?")
+		data = json.load(file)
+		file.close()
+		try:
+			team = data["Teams"][name]
+		except ValueError:
+			return await ctx.send("There are no team with that name here-nyan.")
+		await ctx.send(f"Okay-nyan. <@"+str(team["Leader"])+">, there are a newbie. Do you want to accept them-nyan?")
+		def check(m):
+			return m.author.id == team["Leader"] and m.channel == ctx.channel
+		answer = await self.bot.wait_for('message', check=check)
+		if "yes" in answer.content:
+			role = ctx.guild.get_role(team["TeamRole"])
+			await ctx.author.add_roles(role)
+			await ctx.send(f"Congrats and welcome to the {name} team-nyan!")
+		else:
+			await ctx.send(f"Sorry, {ctx.author.mention}, but <@"+str(team["Leader"])+"> said you can't.")
+
+def setup(bot):
+	bot.add_cog(StarColorPensTeams(bot))
+>>>>>>> 043eed655b20a4df2553c6da3f77d806474cce07
