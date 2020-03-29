d4ae58e2df1cc25e90795ce302f5b579d7c95675
LiloviaLilossy <ddlc.femc@gmail.com>
2020-03-29 09:00:49 +0300

    fully rewrote the help cmd

--- cogs/help.py
+++ cogs/help.py
@@ -28,11 +28,11 @@ class HelpCog(commands.Cog):
         #        if help_value != "":
         #            embed.add_field(name=c.name, value=help_value)
         embed.add_field(name=ctx.prefix+"help colorpens", value="Star Color Pens commands. For most of them bot requires"
-            " `Manage Roles` and `Manage Channels` permissions.")
-        embed.add_field(name=ctx.prefix+"help fun", value="Fun commands.")
-        embed.add_field(name=ctx.prefix+"help misc", value="Misc commands.")
-        embed.add_field(name=ctx.prefix+"help mod", value="Moderation commands.")
-        embed.add_field(name=ctx.prefix+"help owner", value="Owner commands. Only the bot owner can invoke commands in this category.")
+            " `Manage Roles` and `Manage Channels` permissions.", inline=False)
+        embed.add_field(name=ctx.prefix+"help fun", value="Fun commands.", inline=False)
+        embed.add_field(name=ctx.prefix+"help misc", value="Misc commands.", inline=False)
+        embed.add_field(name=ctx.prefix+"help mod", value="Moderation commands.", inline=False)
+        embed.add_field(name=ctx.prefix+"help owner", value="Owner commands. Only the bot owner can invoke commands in this category.", inline=False)
         await ctx.send(embed=embed)
 
     @help.command(name="colorpens")
@@ -69,6 +69,57 @@ class HelpCog(commands.Cog):
             else:
                 help_value += f"`{ctx.prefix}{cmd}`\n"
         embed.add_field(name="Fun commands!", value=help_value)
+        await ctx.send(embed=embed)
+
+    @help.command(name="misc")
+    async def help_misc(self, ctx):
+        embed = Embed(colour=self.bot.defaultcolor)
+        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+        embed.set_footer(text="Nyan! Blue Cat-bot v1.0")
+        embed.set_image(url="https://media.discordapp.net/attachments/616645258280828949/691873653188460584/botinfo.png")
+        cog = self.bot.get_cog("Misc")
+        cmds = cog.get_commands()
+        help_value = ""
+        for cmd in cmds:
+            if cmd.aliases:
+                help_value += f"`{ctx.prefix}{cmd}|"+"|".join(cmd.aliases)+"`\n"
+            else:
+                help_value += f"`{ctx.prefix}{cmd}`\n"
+        embed.add_field(name="Misc commands!", value=help_value)
+        await ctx.send(embed=embed)
+
+    @help.command(name="mod")
+    async def help_mod(self, ctx):
+        embed = Embed(colour=self.bot.defaultcolor)
+        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+        embed.set_footer(text="Nyan! Blue Cat-bot v1.0")
+        embed.set_image(url="https://media.discordapp.net/attachments/616645258280828949/691873653188460584/botinfo.png")
+        cog = self.bot.get_cog("Moderation")
+        cmds = cog.get_commands()
+        help_value = ""
+        for cmd in cmds:
+            if cmd.aliases:
+                help_value += f"`{ctx.prefix}{cmd}|"+"|".join(cmd.aliases)+"`\n"
+            else:
+                help_value += f"`{ctx.prefix}{cmd}`\n"
+        embed.add_field(name="Moderation commands!", value=help_value)
+        await ctx.send(embed=embed)
+
+    @help.command(name="owner")
+    async def help_owner(self, ctx):
+        embed = Embed(colour=self.bot.defaultcolor)
+        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+        embed.set_footer(text="Nyan! Blue Cat-bot v1.0")
+        embed.set_image(url="https://media.discordapp.net/attachments/616645258280828949/691873653188460584/botinfo.png")
+        cog = self.bot.get_cog("Owner")
+        cmds = cog.get_commands()
+        help_value = ""
+        for cmd in cmds:
+            if cmd.aliases:
+                help_value += f"`{ctx.prefix}{cmd}|"+"|".join(cmd.aliases)+"`\n"
+            else:
+                help_value += f"`{ctx.prefix}{cmd}`\n"
+        embed.add_field(name="Owner commands! They can be invoked only by bot owner.", value=help_value)
         await ctx.send(embed=embed)
 
 def setup(bot):
