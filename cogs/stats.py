c1bf3c9190904436af4d32cf388dc06e34c751a4
LiloviaLilossy <55652497+LiloviaLilossy@users.noreply.github.com>
2020-07-05 11:38:56 +0300

    adding statcord, pre-final commit of v1

---
+++ cogs/stats.py
@@ -0,0 +1,20 @@
+from discord.ext import commands
+from addons.get import get_token
+import statcord
+
+
+class StatcordPost(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.key = get_token("statcord")
+        self.api = statcord.Client(self.bot,self.key)
+        self.api.start_loop()
+
+
+    @commands.Cog.listener()
+    async def on_command(self,ctx):
+        self.api.command_run(ctx)
+
+
+def setup(bot):
+    bot.add_cog(StatcordPost(bot))
